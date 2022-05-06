f85139f8f1e31dbdd4e20639d5748ac05edc7e1d
tomas <tomas@DESKTOP-MV6VH7K>
2022-05-06 10:05:04 +0200

    Add project files.

---
+++ Kalkulator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("skriv inn en verdi/tall: ");
+            Double tall1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Skriv inn regnemetoden(+, -, /, *): ");
+            string Opp = Console.ReadLine();
+
+            Console.Write("skriv inn en verdi/tall: ");
+            Double tall2 = Convert.ToDouble(Console.ReadLine());
+
+
+            if(Opp == "+")
+            {
+                Console.WriteLine(tall1 + tall2);
+            }
+            else if (Opp == "-") 
+            {
+                Console.WriteLine(tall1 - tall2); 
+            }
+            else if(Opp == "/") 
+            {
+                Console.WriteLine(tall1 / tall2);
+            }
+            else if (Opp == "*")
+            {
+                Console.WriteLine(tall1 * tall2);
+            }
+        }
+    }
+}
